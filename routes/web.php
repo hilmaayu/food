41e4d2d5eaa0995fa9f568d68f7c41792efd4890
Hilma Ayu Soliha <hilmaayu0@gmail.com>
2025-11-16 16:39:01 +0700

    Authentication and dashboard role 'Admin'

--- routes/web.php
+++ routes/web.php
@@ -21,22 +21,22 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
-// Route::middleware('admin')->group(function () {
-//     Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
-//     Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
-//     Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
-//     Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
-//     Route::post('/admin/password/update', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
+Route::middleware('admin')->group(function () {
+    Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+    Route::get('/admin/profile', [AdminController::class, 'AdminProfile'])->name('admin.profile');
+    Route::post('/admin/profile/store', [AdminController::class, 'AdminProfileStore'])->name('admin.profile.store');
+    Route::get('/admin/change/password', [AdminController::class, 'AdminChangePassword'])->name('admin.change.password');
+    Route::post('/admin/password/update', [AdminController::class, 'AdminPasswordUpdate'])->name('admin.password.update');
 
-// });
+});
 
-// Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
-// Route::post('/admin/login_submit', [AdminController::class, 'AdminLoginSubmit'])->name('admin.login_submit');
-// Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
-// Route::get('/admin/forget_password', [AdminController::class, 'AdminForgetPassword'])->name('admin.forget_password');
-// Route::post('/admin/password_submit', [AdminController::class, 'AdminPasswordSubmit'])->name('admin.password_submit');
-// Route::get('/admin/reset-password/{token}/{email}', [AdminController::class, 'AdminResetPassword']);
-// Route::post('/admin/reset_password_submit', [AdminController::class, 'AdminResetPasswordSubmit'])->name('admin.reset_password_submit');
+Route::get('/admin/login', [AdminController::class, 'AdminLogin'])->name('admin.login');
+Route::post('/admin/login_submit', [AdminController::class, 'AdminLoginSubmit'])->name('admin.login_submit');
+Route::get('/admin/logout', [AdminController::class, 'AdminLogout'])->name('admin.logout');
+Route::get('/admin/forget_password', [AdminController::class, 'AdminForgetPassword'])->name('admin.forget_password');
+Route::post('/admin/password_submit', [AdminController::class, 'AdminPasswordSubmit'])->name('admin.password_submit');
+Route::get('/admin/reset-password/{token}/{email}', [AdminController::class, 'AdminResetPassword']);
+Route::post('/admin/reset_password_submit', [AdminController::class, 'AdminResetPasswordSubmit'])->name('admin.reset_password_submit');
 
 // Route::get('/client/login', [ClientController::class, 'ClientLogin'])->name('client.login');
 // Route::get('/client/register', [ClientController::class, 'ClientRegister'])->name('client.register');
