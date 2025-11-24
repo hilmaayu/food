14a4cb5b5eb632ce02b019b4731c909114db830e
Hilma Ayu Soliha <hilmaayu0@gmail.com>
2025-11-24 08:35:00 +0700

    CRUD city

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth; 
 use App\Models\Category;
+use App\Models\City;
 use Intervention\Image\ImageManager;
 use Intervention\Image\Drivers\Gd\Driver;
 
@@ -101,6 +102,68 @@ class CategoryController extends Controller
         return redirect()->back()->with($notification);
 
     }
+    // End Method 
+
+    // All city method in here
+    public function AllCity(){
+        $city = City::latest()->get();
+        return view('admin.backend.city.all_city', compact('city'));
+    }
+    // End method
+     public function StoreCity(Request $request){
+
+         City::create([
+                'city_name' => $request->city_name,
+                'city_slug' =>  strtolower(str_replace(' ','-',$request->city_name)), 
+            ]);  
+        
+
+        $notification = array(
+            'message' => 'City Inserted Successfully',
+            'alert-type' => 'success'
+        );
+
+        return redirect()->back()->with($notification);
+                   
+    }
+    // End Method 
+
+    public function EditCity($id){
+        $city = City::find($id);
+        return response()->json($city);
+    }
+     // End Method 
+
+     public function UpdateCity(Request $request){
+        $cat_id = $request->cat_id;
+
+        City::find($cat_id)->update([
+               'city_name' => $request->city_name,
+               'city_slug' =>  strtolower(str_replace(' ','-',$request->city_name)), 
+           ]);  
+       
+
+       $notification = array(
+           'message' => 'City Updated Successfully',
+           'alert-type' => 'success'
+       );
+
+       return redirect()->back()->with($notification);
+                  
+   }
+   // End Method 
+
+   public function DeleteCity($id){
+      City::find($id)->delete();
+
+      $notification = array(
+        'message' => 'City Deleted Successfully',
+        'alert-type' => 'success'
+    );
+
+    return redirect()->back()->with($notification);
+
+   }
     // End Method 
   
 } 
